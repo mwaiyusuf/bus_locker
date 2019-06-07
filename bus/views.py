01e43ff59057c81141014f5758ee0eb54dd8e620
mwaiyusuf <mwaigalo5@gmail.com>
2019-06-07 09:11:23 +0300

    user authentication

--- bus/views.py
+++ bus/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
+from __future__ import unicode_literals 
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http  import HttpResponse, Http404, HttpResponseRedirect, JsonResponse
+from .models import project
 # Create your views here.

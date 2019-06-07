4715d0171dceb5b952a2a30c4f2405f9a7c138f4
mwaiyusuf <mwaigalo5@gmail.com>
2019-06-07 10:40:39 +0300

    admin access

--- bus/admin.py
+++ bus/admin.py
@@ -1,3 +1,20 @@
+from __future__ import unicode_literals
 from django.contrib import admin
+# from .models import Location,tags, Image, Project, Profile, Review
+from .models import Bus
+
+# class ProfileAdmin(admin.ModelAdmin):
+#     list_display = ('user',)
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tags',)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title',)
 
 # Register your models here.
+# admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Bus, BusAdmin)
+# admin.site.register(Image, ImageAdmin)
+# admin.site.register(Location)
+# admin.site.register(tags)
+# admin.site.register(Review, ReviewAdmin)

49669907da7f0e95c2749dd3d1332d159b1c1309
Mwaiyusuf <mwaigalo5@gmail.com>
2020-03-24 23:30:54 +0300

    error on static files in the setting

--- bus_locker-master/buslock/settings.py
+++ bus_locker-master/buslock/settings.py
@@ -134,7 +134,9 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'STATICFILES_DIRS = [
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
